2f9fd4bbe98c4c9ea6c9f086f39e397adfe25754
Juan Diego <jd.leonv07@gmail.com>
2026-04-29 22:08:23 -0500

    Lab 9

---
+++ aircraft_altitude.py
@@ -0,0 +1,17 @@
+from aircraft import Aircraft
+
+model = input("Enter aircraft model:\n")
+plane = Aircraft(model)
+
+while True:
+    command = input("Enter command (A for ascent, D for descent, X to exit):\n")
+    parts = command.split()
+
+    if parts[0] == "X":
+        break
+    elif parts[0] == "A":
+        plane.ascend(int(parts[1]))
+    elif parts[0] == "D":
+        plane.descend(int(parts[1]))
+
+print(f"Final altitude: {plane.altitude} feet")
